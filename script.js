66b1d39ffdbaac3e84510f26a2b456d984731737
Stenbyte <sten.scheifel@gmail.com>
2021-05-28 17:27:52 +0300

    85

--- script.js
+++ script.js
@@ -906,3 +906,16 @@
 // }
 // console.log(str("abcdef"));
 // console.log(str("klmnop"));
+//85
+// function num(arr) {
+//   let result = [0, 0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i % 2) {
+//       result[0] += arr[i];
+//     } else {
+//       result[1] += arr[i];
+//     }
+//   }
+//   return result;
+// }
+// console.log(num([1, 3, 6, 2, 5, 10]));
