bc1c008a74b922fa6b33a51acb0021ffe9cc56a4
Stenbyte <sten.scheifel@gmail.com>
2021-06-11 15:10:06 +0300

    117

--- script.js
+++ script.js
@@ -1362,3 +1362,21 @@
 // }
 // console.log(disible("2#0"));
 // console.log(disible("4#2"));
+//117
+// function matrix(matrix) {
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix.length; j++) {
+//       if ((i === j && matrix[i][j] !== 1) || (matrix[i][j] !== 0 && i !== j)) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+// console.log(
+//   matrix([
+//     [1, 0, 0],
+//     [0, 1, 0],
+//     [0, 0, 1],
+//   ])
+// );
