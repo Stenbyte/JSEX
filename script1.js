d1333a6f9f033b2e13b6be23aeb516ea4ef36ba8
Stenbyte <sten.scheifel@gmail.com>
2022-02-07 18:15:15 +0200

    guess&dice game

--- script1.js
+++ script1.js
@@ -162,3 +162,93 @@
 //     .join("");
 // }
 // console.log(longest("aretheyhere", "yestheyarehere"));
+// document.getElementById("test").innerText = "Go";
+// const btn = document.getElementById("mul");
+// btn.textContent = "check";
+// const numberG = document.querySelector(".number");
+// const mat = Math.trunc(Math.random() * 20) + 1;
+// numberG.textContent = mat;
+
+// btn.addEventListener("click", function () {
+//   const ch = Number(document.getElementById("num1").value);
+
+//   if (!ch) {
+//     document.getElementById("warn").textContent = "Enter a valid number";
+//   } else if (ch === +numberG.textContent) {
+//     document.getElementById("warn").textContent = "Yes";
+//     document.body.style.backgroundColor = "red";
+//   }
+// });
+// const btns = document.querySelectorAll(".show");
+// const modal = document.querySelector(".modal");
+// const close = document.querySelector(".close");
+// const overlay = document.querySelector(".overlay");
+
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//   });
+// }
+// close.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+// overlay.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+//   }
+// });
+/* const newGame = document.querySelector(".new");
+const result1 = document.querySelector(".result");
+const result2 = document.querySelector(".result1");
+const roll = document.querySelector(".roll");
+const hold = document.querySelector(".hold");
+const mainNum = document.querySelector(".number");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+// Start
+let scores = [0, 0];
+let score = 0;
+let activeP = 0;
+const switchP = function () {
+  document.getElementById(`id-${activeP}`).textContent = 0;
+  score = 0;
+  activeP = activeP === 0 ? 1 : 0;
+  left.classList.toggle("back");
+  right.classList.toggle("back");
+};
+roll.addEventListener("click", function () {
+  const randomN = Math.trunc(Math.random() * 6) + 1;
+  mainNum.textContent = randomN;
+  if (randomN !== 1) {
+    score += randomN;
+    document.getElementById(`id-${activeP}`).textContent = score;
+  } else {
+    // switch
+    switchP();
+  }
+});
+// Hold function
+hold.addEventListener("click", function () {
+  scores[activeP] += score;
+  document.getElementById(`total-${activeP}`).textContent = scores[activeP];
+  if (scores[activeP] >= 10) {
+    document.getElementById(`left-${activeP}`).style.backgroundColor =
+      "crimson";
+  } else {
+    switchP();
+  }
+});
+newGame.addEventListener("click", function () {
+  scores = [0, 0];
+  document.getElementById(`total-0`).textContent = 0;
+  document.getElementById(`total-1`).textContent = 0;
+});
+ */
